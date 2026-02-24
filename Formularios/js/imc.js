7f278e43aa1cb7371621a233dc87fabce1760730
Verónica Mora Lezcano <vmora@ucenfotec.ac.cr>
2026-02-24 16:30:35 -0600

    [new] Constantes y comentarios en JS

---
+++ Formularios/js/imc.js
@@ -0,0 +1,25 @@
+
+/*
+Comentario
+multilínea
+*/
+
+// Comentario de una línea
+
+// console.log es equivalente a print() en Python: Mensaje en consola
+console.log("Mensaje en la consola");
+console.log("Otro mensaje en la consola");
+
+// Constante: Valor que no cambia durante la ejecución de un programa. 
+const PI = 3.14;
+console.log(PI);
+
+const MAX_USUARIOS = 100;
+console.log(MAX_USUARIOS);
+
+const GRAVEDAD = 9.18;
+
+const TIPO_USUARIO = "Visitante"; 
+console.log(TIPO_USUARIO);
+
+// Variable: Valor cambia durante la ejecución de un programa. 
