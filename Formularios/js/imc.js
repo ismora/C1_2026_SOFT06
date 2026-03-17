cd0a9fa371a51ddff173f96ef6f6c8596979765c
Verónica Mora Lezcano <vmora@ucenfotec.ac.cr>
2026-03-17 13:30:22 -0600

    [new]: Validación de campos vacíos

--- Formularios/js/imc.js
+++ Formularios/js/imc.js
@@ -28,8 +28,33 @@ function calcularIMCRetorno(){
     let estatura = txtEstatura.value;
     
     let imc = peso / Math.pow(estatura, 2);
-    parrafoResultado.innerText = imc.toFixed(2);
+    
+    if (validarCamposVacios() === false){
+        parrafoResultado.innerText = imc.toFixed(2);
+    }
+    else{
+        parrafoResultado.innerText = "No se puede calcular el IMC: Por favor revise los campos resaltados";
+    }
 
+}
+
+function validarCamposVacios(){
+    let error = false; // Por defecto que no existen errores 
+    if (txtPeso.value === ""){
+        txtPeso.classList.add("input-error");
+        error = true;
+    }
+    else{
+        txtPeso.classList.remove("input-error")
+    }
+    if (txtEstatura.value === ""){
+        txtEstatura.classList.add("input-error");
+        error = true;
+    }
+    else{
+        txtEstatura.classList.remove("input-error")
+    }
+    return error;
 }
 
 
